bee9ea2ae0d7e94017f3dd392a630bd6c3a972d9
IKende <henryfan@msn.com>
2019-02-27 17:19:31 +0800

    add websocket sample

---
+++ samples/HttpApiServer.WebSocket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HttpApiServer.WebSocket
+{
+    [BeetleX.FastHttpApi.Controller]
+    class Program
+    {
+        private static BeetleX.FastHttpApi.HttpApiServer mApiServer;
+
+        static void Main(string[] args)
+        {
+            mApiServer = new BeetleX.FastHttpApi.HttpApiServer();
+            mApiServer.Debug();
+            mApiServer.Register(typeof(Program).Assembly);
+            mApiServer.Open();
+            mApiServer.WebSocketReceive = (o, e) =>
+            {
+                Console.WriteLine(e.Frame.Body);
+                var freame = e.CreateFrame($"{DateTime.Now}" + e.Frame.Body.ToString());
+                e.Response(freame);
+            };
+            Console.Write(mApiServer.BaseServer);
+            Console.Read();
+        }
+
+        public string Hello(string name)
+        {
+            return $"{name} {DateTime.Now}";
+        }
+    }
+}
