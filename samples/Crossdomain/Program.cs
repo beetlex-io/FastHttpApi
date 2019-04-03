e23646d2702cf0d531b8df48a1d9728577b92b59
IKende <henryfan@msn.com>
2019-04-03 20:11:46 +0800

    add cross domain sample

---
+++ samples/Crossdomain/Program.cs
@@ -0,0 +1,36 @@
+﻿using BeetleX.FastHttpApi;
+using System;
+
+namespace Crossdomain
+{
+    [Options(AllowOrigin = "*")]
+    [BeetleX.FastHttpApi.Controller]
+    class Program
+    {
+        private static HttpApiServer mApiServer;
+
+        static void Main(string[] args)
+        {
+            mApiServer = new HttpApiServer();
+            mApiServer.Options.LogToConsole = true;
+            mApiServer.Options.LogLevel = BeetleX.EventArgs.LogType.Debug;
+            mApiServer.Debug();
+            mApiServer.Register(typeof(Program).Assembly);
+            mApiServer.Open();
+            Console.Write(mApiServer.BaseServer);
+            Console.WriteLine(Environment.ProcessorCount);
+            Console.Read();
+        }
+
+        public string hello(string name)
+        {
+            return $"hello {name} {DateTime.Now}";
+        }
+
+        [Post]
+        public string post(string name)
+        {
+            return $"post {name} {DateTime.Now}";
+        }
+    }
+}
